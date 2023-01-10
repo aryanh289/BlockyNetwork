0f2ee87c6e89896b75595075dc0618a7a1971d8d
aryanh289 <42418494+aryanh289@users.noreply.github.com>
2023-01-11 04:20:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dapp University
+          BlockyNetwork
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
